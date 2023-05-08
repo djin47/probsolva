06d00eaad25ed7a31cd5f0f68e77bcfd53901472
Jindal <dhjindal@deloitte.com>
2023-05-08 09:43:14 +0530

    Day 2 final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./styles/app.scss";
-import "./styles/header.scss";
+import "./styles/header.scss"
+import "./styles/home.scss"
 
 function App() {
   return (
+  <>
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element = {<Home/>}/>
+        <Route path="/" element = {<Home />} />
       </Routes>
     </Router>
-  );
+  </>
+  )
 }
-
 export default App;
